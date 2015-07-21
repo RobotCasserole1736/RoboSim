c6a9b69b868cf61b00c3fbc196ae5be23ec798a9
Chris Gerth <chrisgerth010592@gmail.com>
2015-07-20 20:41:26 -0500

    more gui WIP. need to add inputs and serial read/write code

--- Octave/RoboSim_test/gui_callbacks.m
+++ Octave/RoboSim_test/gui_callbacks.m
@@ -35,7 +35,7 @@ function gui_callbacks(source, callbackdata)
     btn_DO4_callback(source,callbackdata);
   elseif(source == btn_DO5)
     btn_DO5_callback(source,callbackdata);
-  elseif(source == btn_DO60)
+  elseif(source == btn_DO6)
     btn_DO6_callback(source,callbackdata);
   elseif(source == btn_DO7)
     btn_DO7_callback(source,callbackdata);
@@ -56,41 +56,81 @@ function a2_sld_callback(source,callbackdata)
 end
 
 function btn_DO0_callback(source, callbackdata)
-  disp('called DO 0 callback')
-  fflush(stdout);
+  global digital_outputs;
+  digital_outputs(1) = ~digital_outputs(1);
+  if(digital_outputs(1))
+    set(source,'Backgroundcolor','g');
+  else
+    set(source,'Backgroundcolor','r');
+  end
 end
 
 function btn_DO1_callback(source, callbackdata)
-  disp('called DO 1 callback')
-  fflush(stdout);
+  global digital_outputs;
+  digital_outputs(2) = ~digital_outputs(2);
+  if(digital_outputs(2))
+    set(source,'Backgroundcolor','g');
+  else
+    set(source,'Backgroundcolor','r');
+  end
 end
 
 function btn_DO2_callback(source, callbackdata)
-  disp('called DO 2 callback')
-  fflush(stdout);
+  global digital_outputs;
+  digital_outputs(3) = ~digital_outputs(3);
+  if(digital_outputs(3))
+    set(source,'Backgroundcolor','g');
+  else
+    set(source,'Backgroundcolor','r');
+  end
 end
 
 function btn_DO3_callback(source, callbackdata)
-  disp('called DO 3 callback')
-  fflush(stdout);
+  global digital_outputs;
+  digital_outputs(4) = ~digital_outputs(4);
+  if(digital_outputs(4))
+    set(source,'Backgroundcolor','g');
+  else
+    set(source,'Backgroundcolor','r');
+  end
 end
 
 function btn_DO4_callback(source, callbackdata)
-  disp('called DO 4 callback')
-  fflush(stdout);
+  global digital_outputs;
+  digital_outputs(5) = ~digital_outputs(5);
+  if(digital_outputs(5))
+    set(source,'Backgroundcolor','g');
+  else
+    set(source,'Backgroundcolor','r');
+  end
 end
 
 function btn_DO5_callback(source, callbackdata)
-  disp('called DO 5 callback')
-  fflush(stdout);
+  global digital_outputs;
+  digital_outputs(6) = ~digital_outputs(6);
+  if(digital_outputs(6))
+    set(source,'Backgroundcolor','g');
+  else
+    set(source,'Backgroundcolor','r');
+  end
 end
 
 function btn_DO6_callback(source, callbackdata)
-  disp('called DO 6 callback')
-  fflush(stdout);
+  global digital_outputs;
+  digital_outputs(7) = ~digital_outputs(7);
+  if(digital_outputs(7))
+    set(source,'Backgroundcolor','g');
+  else
+    set(source,'Backgroundcolor','r');
+  end
 end
 
 function btn_DO7_callback(source, callbackdata)
-  disp('called DO 7 callback')
-  fflush(stdout);
+  global digital_outputs;
+  digital_outputs(8) = ~digital_outputs(8);
+  if(digital_outputs(8))
+    set(source,'Backgroundcolor','g');
+  else
+    set(source,'Backgroundcolor','r');
+  end
 end
