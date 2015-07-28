1128266151494ca3734aa63767c55e4a4585ab0b
Chris Gerth <chrisgerth010592@gmail.com>
2015-07-27 23:32:58 -0500

    Got rtos pretty much working. Need to tweak stack size parameters and add comments so I remember what the heck I did this late on a work night

---
+++ ArduinoSrc_RTOS/RoboSim_rtos/plant.ino
@@ -0,0 +1,33 @@
+/******************************************************************************/
+/******************************************************************************/
+/**   COPYRIGHT (C) FRC TEAM 1736 ROBOT CASSEROLE 2015 ALL RIGHTS RESERVED   **/
+/******************************************************************************/
+/******************************************************************************/
+// RoboSim - an Arduino-based, Hardware-in-the-loop Robot simulation platform
+//
+// File: plant.cpp
+// Description:  - Main plant function for emulating robot functionality
+//
+//  Change History:
+//      Chris Gerth - 20Mar2015 - Created
+//
+/******************************************************************************/
+
+#include "plant.h"
+
+void plant_periodic_loop()
+{ 
+  /*
+  analog_outputs[0] = 1.14;
+  analog_outputs[1] = 2.14;
+  digital_outputs[0] = true;
+  digital_outputs[1] = false;
+  digital_outputs[2] = true;
+  digital_outputs[3] = false;
+  digital_outputs[4] = true;
+  digital_outputs[5] = false;
+  digital_outputs[6] = true;
+  digital_outputs[7] = false;
+  */
+
+}
