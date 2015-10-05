466d87d2856cb0dcaf4f1ab68f5e7664ecec6523
Chris Gerth <Gerth_Christopher_M@cat.com>
2015-10-05 07:26:22 -0500

    removed old codebases

--- ArduinoSrc_RTOS/RoboSim_rtos/idlehook.c
+++
@@ -1,7 +0,0 @@
-void loop();
-void __attribute__((weak)) vApplicationIdleHook() {
-  //loop(); Don't do anything in the idle hook
-  for(;;);
-}
-void __attribute__((weak)) vApplicationTickHook() {
-}
