0103ac6d22cadb307e2f5596f56faf1324f35ad3
Chris Gerth <chrisgerth010592@gmail.com>
2015-11-01 08:19:56 -0600

    Moved motor sim model back to standard lib/lib_test folders

--- Octave/lib/motor_CIM.m
+++ Octave/lib/motor_CIM.m
@@ -18,29 +18,9 @@ Lc = 0.02; %henries, just a guess based on what made things look nice
 Kt = 2.429/131.227; %Nm/A - for CIM motor. Calculated from Stall Torque/Stall Current
 Ki = (12-(2.7*Rc))/(5310*2*pi/60); %V/(rad/s). Calculated from Vemf@FreeSpeed/(2pi/60*RPM@FreeSpeed). Steady-state Vemf = Vs - I@FreeSpeed*Rc, for Vs = 12
 
-%The following are again total guesses, this data is not available anywhere I can find.
-Kf = 0.25; %Kinetic rotational coefficent of friction in Nm-s for motor
-Tfs = 0.2; %Static friction maximum torque magnititude
-speed_mag_zero_limit = 10; %in rad/sec This and below define pratical deadzones for speed 
-
-SystemMomentOfInertia = 0.555 * 0.0254^2; % kg-m^2 - just a guess the rotating member of CIM - model as 1.25lb, 1 in. radius solid cylinder
-
 % torque calculation
 torque = torque_prev + Ts*(Kt/Lc * (voltage - torque_prev*Rc/Kt - Ki*speed_prev));
 
 %current calculation
 current = torque / Kt;
 
-%If motor is rotating, speed is increased by torque and decreased by friction
-if(abs(speed_prev) > speed_mag_zero_limit)
-    speed = speed_prev + Ts*(1/SystemMomentOfInertia * (torque_prev) - Kf*speed_prev );
-%If motor is stopped  
-else
-    %If torque has exceeded static friction, the motor may spin per normal equations
-    if(abs(torque_prev) > Tfs )
-        speed = speed_prev + Ts*(1/SystemMomentOfInertia * (torque_prev) - Kf*speed_prev);
-    %Else, motor sticks and speed remains zero
-    else
-        speed = 0;
-    end
-end
--- Octave/lib_test/motor_model_test.m
+++ Octave/lib_test/motor_model_test.m
@@ -2,22 +2,18 @@ clear;
 clc;
 clf;
 
-%Constants, motor-specific.
-%See http://files.andymark.com/CIM-motor-curve-am-0255.pdf
-Rc = 0.3; %ohms, for full CIM motor. Didn't have an Ohmmeter that could measure this, so just kinda guessed.
-Lc = 0.02; %henries, just a guess based on what made things look nice
-Kt = 2.429/131.227; %Nm/A - for CIM motor. Calculated from Stall Torque/Stall Current
-Ki = (12-(2.7*Rc))/(5310*2*pi/60); %V/(rad/s). Calculated from Vemf@FreeSpeed/(2pi/60*RPM@FreeSpeed). Steady-state Vemf = Vs - I@FreeSpeed*Rc, for Vs = 12
+global Ts
+
+addpath('../lib');
 
 %The following are again total guesses, this data is not available anywhere I can find.
 Kf = 0.25; %Kinetic rotational coefficent of friction in Nm-s for motor
 Tfs = 0.2; %Static friction maximum torque magnititude
 speed_mag_zero_limit = 10; %in rad/sec This and below define pratical deadzones for speed 
 
-
 SystemMomentOfInertia = 0.555 * 0.0254^2; % kg-m^2 - just a guess the rotating member of CIM - model as 1.25lb, 1 in. radius solid cylinder
 LoadTorque = 0; %Nm
-Ts = 0.01; %50 ms sample rate
+Ts = 0.01; %10 ms sample rate
 
 
 step_time = 1;
@@ -33,10 +29,12 @@ num_samples = length(voltage);
 time = (0:1:num_samples-1)*Ts;
 torque = zeros(1,num_samples);
 speed = zeros(1,num_samples);
+current = zeros(1,num_samples);
 
 for i = 1:1:num_samples-1
-    torque(i+1) = torque(i) + Ts*(Kt/Lc * (voltage(i) - torque(i)*Rc/Kt - Ki*speed(i)));
+    current(i+1), torque(i+1) = motor_CIM(voltage(i), torque(i), speed(i));
     
+    %plant-ish model of the rotating member & bearings
     %If motor is rotating, speed is increased by torque and decreased by friction
     if(abs(speed(i)) > speed_mag_zero_limit)
       speed(i+1) = speed(i) + Ts*(1/SystemMomentOfInertia * (torque(i)-LoadTorque) - Kf*speed(i) );
@@ -54,16 +52,18 @@ for i = 1:1:num_samples-1
     
 end
 
-subplot(3,1,1);
+subplot(4,1,1);
 plot(time, voltage);
 title('Voltage (V) vs. Time (S)');
-subplot(3,1,2);
+subplot(4,1,2);
 plot(time, torque);
 title('Torque (Nm) vs. Time (S)');
-subplot(3,1,3);
+subplot(4,1,3);
 plot(time, speed*60/2/pi);
 title('Speed (RPM) vs. Time (S)');
-
+subplot(4,1,4);
+plot(time, current);
+title('Current (I) vs. Time (S)');
 
 
 
